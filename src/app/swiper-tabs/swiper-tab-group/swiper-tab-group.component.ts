ec3d9efd932abd7678f72a4b3213d6d681e71429
leesherwood28 <leesherwood28@googlemail.com>
2021-08-08 14:55:14 +0100

    6 files modified

--- src/app/swiper-tabs/swiper-tab-group/swiper-tab-group.component.ts
+++ src/app/swiper-tabs/swiper-tab-group/swiper-tab-group.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { SwiperTabComponent } from '../swiper-tab/swiper-tab.component';
 
 @Component({
   selector: 'app-swiper-tab-group',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swiper-tab-group.component.css']
 })
 export class SwiperTabGroupComponent implements OnInit {
+  activeIndex$ = new BehaviorSubject<number>(0);
+
+  @ContentChildren(SwiperTabComponent) tabs: QueryList<SwiperTabComponent>;
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/swiper-tabs/swiper-tab-header/swiper-tab-header.component.ts
+++ src/app/swiper-tabs/swiper-tab-header/swiper-tab-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-swiper-tab-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swiper-tab-header.component.css']
 })
 export class SwiperTabHeaderComponent implements OnInit {
+  @Input() label: string;
+
   constructor() {}
 
   ngOnInit() {}
