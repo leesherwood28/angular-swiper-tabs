b43622be5e5ed494d93aafdaa94045be49d971f2
leesherwood28 <leesherwood28@googlemail.com>
2021-08-08 12:42:24 +0100

    3 files added

---
+++ src/app/swiper-tabs/swiper-tab-group/swiper-tab-group.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swiper-tab-group',
+  templateUrl: './swiper-tab-group.component.html',
+  styleUrls: ['./swiper-tab-group.component.css']
+})
+export class SwiperTabGroupComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
