3e9b9bfc36a956f97b45b5083a7d6efacc5b44b4
leesherwood28 <leesherwood28@googlemail.com>
2021-08-08 15:18:37 +0100

    6 files modified

--- src/app/swiper-tabs/swiper-tab/swiper-tab.component.ts
+++ src/app/swiper-tabs/swiper-tab/swiper-tab.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import {
+  Component,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-swiper-tab',
@@ -7,6 +14,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SwiperTabComponent implements OnInit {
   @Input() label: string;
+
+  @ViewChild(TemplateRef, { static: true }) contentRef;
 
   constructor() {}
 
