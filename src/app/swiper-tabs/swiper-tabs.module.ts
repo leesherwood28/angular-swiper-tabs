f979ea472869acd39d7681c269e0f7bf029874b8
leesherwood28 <leesherwood28@googlemail.com>
2021-08-08 15:12:08 +0100

    4 files modified

--- src/app/swiper-tabs/swiper-tabs.module.ts
+++ src/app/swiper-tabs/swiper-tabs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SwiperTabGroupComponent } from './swiper-tab-group/swiper-tab-group.component';
 import { SwiperTabComponent } from './swiper-tab/swiper-tab.component';
 import { SwiperTabHeaderComponent } from './swiper-tab-header/swiper-tab-header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   declarations: [
     SwiperTabGroupComponent,
     SwiperTabComponent,
