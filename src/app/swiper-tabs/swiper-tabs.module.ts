37998a5e5f867686a017697e7c19eb33326c1eee
leesherwood28 <leesherwood28@googlemail.com>
2021-08-08 12:52:11 +0100

    1 file modified

--- src/app/swiper-tabs/swiper-tabs.module.ts
+++ src/app/swiper-tabs/swiper-tabs.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwiperTabGroupComponent } from './swiper-tab-group/swiper-tab-group.component';
+import { SwiperTabComponent } from './swiper-tab/swiper-tab.component';
+import { SwiperTabHeaderComponent } from './swiper-tab-header/swiper-tab-header.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
-  providers: []
+  declarations: [
+    SwiperTabGroupComponent,
+    SwiperTabComponent,
+    SwiperTabHeaderComponent
+  ],
+  providers: [],
+  exports: [SwiperTabGroupComponent, SwiperTabComponent]
 })
 export class SwiperTabsModule {}
