21fdb9e5e7c27686953454b2bea8fdcb70872aa0
leesherwood28 <leesherwood28@googlemail.com>
2021-08-16 11:33:16 +0100

    Setting up pan state

--- src/app/swiper-tabs/swiper-tab-group/swiper-tab-group.component.ts
+++ src/app/swiper-tabs/swiper-tab-group/swiper-tab-group.component.ts
@@ -8,10 +8,31 @@ import {
   ViewChildren
 } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { BehaviorSubject, Subject, combineLatest } from 'rxjs';
-import { debounceTime, map, tap } from 'rxjs/operators';
+import {
+  BehaviorSubject,
+  Subject,
+  combineLatest,
+  OperatorFunction,
+  Observable,
+  merge
+} from 'rxjs';
+import { debounceTime, map, scan, startWith, tap } from 'rxjs/operators';
 import { SwiperTabHeaderComponent } from '../swiper-tab-header/swiper-tab-header.component';
 import { SwiperTabComponent } from '../swiper-tab/swiper-tab.component';
+
+export interface Pan {
+  position: number;
+  velocity: number;
+  isPanning: boolean;
+}
+
+export interface State {
+  activeIndex: number;
+  isPanning: boolean;
+  panPosition?: number;
+  hostWidth?: number;
+  headerWidth?: number;
+}
 
 @Component({
   selector: 'app-swiper-tab-group',
@@ -19,21 +40,41 @@ import { SwiperTabComponent } from '../swiper-tab/swiper-tab.component';
   styleUrls: ['./swiper-tab-group.component.css']
 })
 export class SwiperTabGroupComponent implements OnInit {
-  readonly activeIndex$ = new BehaviorSubject<number>(0);
-
+  // Input observables
+  readonly requestedIndex$ = new Subject<number>();
   readonly hostWidth$ = new Subject<number>();
   readonly headerWidth$ = new Subject<number>();
+  readonly panning$ = new Subject<Pan>();
 
-  readonly tabTranslateX$ = combineLatest([
-    this.activeIndex$,
+  // State
+  readonly initState: State = { activeIndex: 0, isPanning: false };
+
+  readonly state$ = merge(
+    this.requestedIndex$.pipe(map(activeIndex => ({ activeIndex }))),
+    this.hostWidth$.pipe(map(hostWidth => ({ hostWidth }))),
+    this.headerWidth$.pipe(map(headerWidth => ({ headerWidth }))),
+    this.panning$.pipe(
+      map(pan => ({ isPanning: pan.isPanning, panPosition: pan.position }))
+    )
+  ).pipe(scan((acc, curr) => ({...acc, ...curr}), this.initState));
+
+  // Outputs
+
+  readonly tabTranslateX1$ = combineLatest([
+    this.requestedIndex$,
     this.hostWidth$
   ]).pipe(
     map(([activeIndex, hostWidth]) => -activeIndex * hostWidth),
     debounceTime(0)
   );
 
+  readonly tabTranslateX$ = combineLatest([this.panning$]).pipe(
+    map(([e]) => e.position),
+    debounceTime(0)
+  );
+
   readonly headerTranslateX$ = combineLatest([
-    this.activeIndex$,
+    this.requestedIndex$,
     this.headerWidth$
   ]).pipe(
     map(([activeIndex, headerWidth]) => activeIndex * headerWidth),
@@ -70,10 +111,15 @@ export class SwiperTabGroupComponent implements OnInit {
   }
 
   setActiveIndex(index: number) {
-    this.activeIndex$.next(index);
+    this.requestedIndex$.next(index);
   }
 
   onPan(e) {
+    this.panning$.next({
+      position: e.deltaX,
+      isPanning: !e.isFinal,
+      velocity: e.velocityX
+    });
     console.log(e);
   }
 }
